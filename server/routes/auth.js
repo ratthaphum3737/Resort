811acbf2c2a6f21160766b75b5b0730f0f138ed2
ratthaphum3737 <ratthaphum.f@kkumail.com>
2026-03-09 17:31:04 +0700

    update mail system

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,8 +4,12 @@ const nodemailer = require("nodemailer");
 const router = express.Router();
 const pool = require('../db');
 
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
